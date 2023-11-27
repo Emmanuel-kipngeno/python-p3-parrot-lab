3c7d826e76ceea54947b3b9b14499f6c6e931d1e
emmanuel-kipngeno <immanuelkipngeno7387@gmail.com>
2023-11-27 12:17:21 +0300

    completed assignment

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,10 @@
-def parrot():
-    pass
+def parrot(sound="Squawk!"):
+    print(sound)
+    return sound
+
+# Example Usage:
+default_sound = parrot()  # Output: Squawk!
+custom_sound = parrot("Hello, world!")  # Output: Hello, world!
+
+print("Default Sound:", default_sound)
+print("Custom Sound:", custom_sound)
